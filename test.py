942effc0534f71072516f4ac915fb2ccfa8ced53
Snipy7374 <snipy7374@gmail.com>
2022-12-13 23:18:56 +0100

    load a primitive example

---
+++ test.py
@@ -0,0 +1,9 @@
+from nasa import Client
+import requests
+import io
+
+
+c = Client(token="TOKEN_HERE")
+img = c.get_astronomy_picture("2022-12-13")
+
+asset = img.image.save("image.png", seek_at_end=False)
