325142c3c0c64a36de57f9b46e6217b5dceff3c6
Ihor Tymkiv <102431570+GlitchingCore@users.noreply.github.com>
2023-02-20 22:37:20 +0200

    Propose two variations of caching decorator.

--- demo/redis_cache.py
+++ demo/redis_cache.py
@@ -4,8 +4,9 @@ from inspect import getfullargspec
 
 # change redis to redis host
 redis = Redis(host="redis", port=6379)
+TTL = 3600 # in seconds
 
-def cache(key: str):
+def cache_variation_1(key: str):
     """
     Redis cache decorator that optionally injects two arguments into the wrapped function.
 
@@ -24,3 +25,19 @@ def cache(key: str):
             return func(*args, **kwargs)
         return wrapper
     return decorator
+
+
+def cache_variation_2(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = '-'.join([func.__name__] + list(args))
+        cache = redis.get(key)
+
+        if cache is None:
+            value = func(*args, **kwargs)
+            redis.set(name=key, value=value, ex=TTL)
+        else:
+            value = cache
+        
+        return value
+    return wrapper
