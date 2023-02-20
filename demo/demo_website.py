a762c6924a05e6671bd270fe3635129e4f5f86e3
Ihor Tymkiv <102431570+GlitchingCore@users.noreply.github.com>
2023-02-20 22:21:43 +0200

    Use caching for apod endpoint

--- demo/demo_website.py
+++ demo/demo_website.py
@@ -4,11 +4,20 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template
 
+from redis_cache import redis, cache
+
 client = NasaSyncClient(token="DEMO_KEY")
 app = Flask(__name__)
 
+REDIS_EXPIRE_TIME_IN_SECONDS = 3600
+
 @app.route("/")
-def apod():
-    apod = client.get_astronomy_picture()
+@cache("apod")
+def apod(cache, key):
+    if cache is None:
+        apod = client.get_astronomy_picture()
+        redis.set(name=key, value=apod, ex=REDIS_EXPIRE_TIME_IN_SECONDS)
+    else:
+        apod = cache
     return render_template("test.html", apod=apod, date=datetime.strftime(apod.date, "%Y-%m-%d"))
 
